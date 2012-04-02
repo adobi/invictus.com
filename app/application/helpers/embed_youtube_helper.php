6d727c5f4676ed7c95d91757d91c814847eaebf7
Attila Dobi <hello@adobi.hu>
2012-04-02 20:24:31 +0200

    responsive video

--- app/application/helpers/embed_youtube_helper.php
+++ app/application/helpers/embed_youtube_helper.php
@@ -4,7 +4,10 @@
 	
 		function embed_youtube($video, $autoplay = false, $width = 340, $height = 260) {
             
-			return '<iframe width="'.$width.'" height="'.$height.'" src="http://www.youtube.com/embed/'.$video.'/?wmode=opaque'.($autoplay ? 'autoplay=1&' : '') .'" frameborder="0" allowfullscreen></iframe>';
+			return '<div class="video-wrapper"><div class="video-container">'.
+			  '<iframe width="'.$width.'" height="'.$height.'" src="http://www.youtube.com/embed/'.$video.'/?wmode=opaque'.($autoplay ? 'autoplay=1&' : '') .'" frameborder="0" allowfullscreen></iframe>'
+			  .'</div></div>';
+			  
 		}	
 	}
 
