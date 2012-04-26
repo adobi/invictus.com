34ee7051dfe2a4bb26351e69b1df2e4451bf99c6
Attila Dobi <hello@adobi.hu>
2012-04-26 15:45:53 +0200

    game link to microsite and unset session

--- app/application/views/game/publish_final.php
+++ app/application/views/game/publish_final.php
@@ -34,7 +34,12 @@
         <a class="btn btn-large span4" href="<?php echo PRESS_RELEASE_URL ?>auth/auto_login/<?php echo $this->session->userdata('created_press_release_id') ? '?r=pressrelease/edit/'.$this->session->userdata('created_news_id') : '' ?>" target="_blank">Go to "Invictus Press"</a>
       </div>
       <div class="span4" style="margin-bottom:20px;">
-        <a class="btn btn-large span4" href="#" target="_blank">Go to "Invictus Microsites"</a>
+        <a class="btn btn-large span4" href="<?php echo MICROSITES_URL ?>auth/auto_login/<?php echo $this->session->userdata('created_microsite_id') ? '?r=microsite/edit/'.$this->session->userdata('created_microsite_id') : '' ?>" target="_blank">Go to "Invictus Microsites"</a>
       </div>      
     </div>
-  <?php echo form_close() ?>    
+  <?php echo form_close() ?> 
+  <?php 
+    $this->session->unset_userdata('created_news_id');
+    $this->session->unset_userdata('created_press_release_id');
+    $this->session->unset_userdata('created_microsite_id')
+  ?>   
