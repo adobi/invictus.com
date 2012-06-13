437cb3eada93ef4c08fdd85cec5d8b68c7c86085
Attila Dobi <hello@adobi.hu>
2012-06-13 11:25:23 +0200

    api index fix

--- app/application/views/api/index.php
+++ app/application/views/api/index.php
@@ -1,7 +1,7 @@
 <h1>Invictus Games public API</h1>
 <hr>
 <div class="btn-group">
-  <a href="<?php echo base_url() ?>api/games" class="btn btn-large">Platforms</a>
-  <a href="<?php echo base_url() ?>api/platforms" class="btn btn-large">Games</a>
+  <a href="<?php echo base_url() ?>api/platforms" class="btn btn-large">Platforms</a>
+  <a href="<?php echo base_url() ?>api/games" class="btn btn-large">Games</a>
   <a href="<?php echo base_url() ?>api/game_platforms" class="btn btn-large">Game platforms</a>
 </div>
